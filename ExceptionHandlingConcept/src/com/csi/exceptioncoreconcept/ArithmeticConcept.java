4751935d90943a602d23e87f1888907e1cbdc79c
Nita <nitadake95@gmail.com>
2021-01-15 15:00:56 +0530

    added arithmetic concept

---
+++ ExceptionHandlingConcept/src/com/csi/exceptioncoreconcept/ArithmeticConcept.java
@@ -0,0 +1,13 @@
+package com.csi.exceptioncoreconcept;
+
+public class ArithmeticConcept {
+
+
+public static void main(String[] args) {
+	int a =10;
+	int b=19;
+	int result ;
+	result=a+b;
+	System.out.println(result);
+}
+}
