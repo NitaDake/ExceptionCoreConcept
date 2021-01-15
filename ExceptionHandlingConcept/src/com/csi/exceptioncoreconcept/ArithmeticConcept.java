c0e0b4d4ef1b7ff2e422fc0323e784d5c651b3ad
Nita <nitadake95@gmail.com>
2021-01-15 15:08:09 +0530

    Added arithmetic file

--- ExceptionHandlingConcept/src/com/csi/exceptioncoreconcept/ArithmeticConcept.java
+++ ExceptionHandlingConcept/src/com/csi/exceptioncoreconcept/ArithmeticConcept.java
@@ -2,12 +2,12 @@ package com.csi.exceptioncoreconcept;
 
 public class ArithmeticConcept {
 
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 19;
+		int result;
+		result = a + b;
+		System.out.println(result);
 
-public static void main(String[] args) {
-	int a =10;
-	int b=19;
-	int result ;
-	result=a+b;
-	System.out.println(result);
-}
+	}
 }
